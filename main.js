e7284e37d9501f9dccbe12e961aa5dba0b98bf4f
Chase Coffman <chasecoffman@amdg.rockhursths.edu>
2018-12-04 11:43:30 -0600

    Food Prototype

--- main.js
+++ main.js
@@ -1,12 +1,73 @@
+var sq;
+var feed = []; // an empty array
+var numFood = 10;
+
+var a = [1,2,3];
+
 function setup() {
-    createCanvas(640, 480);
+    createCanvas(740, 480);
+    sq = new Squirrel();
+
+    // initializing 10 pieces of food
+    for(var i = 0; i < numFood; i++) {
+        feed.push(new Food(random(width), random(height)));
+    }
 }
 
 function draw() {
-    background('#FFFAED');
+    background(0,255,0);
+    sq.display();
+    
+    // display all the food
+    for(var i = 0; i < numFood; i++) {
+        feed[i].display();
+    }
+}
+
+function mousePressed() {
+    sq.eat();
+}
+
+function Food(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = color(255, 0, 0);
+    this.foodSize = 50;
+    
+    this.display = function() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.foodSize, this.foodSize);
+    }
+}
+
+function Squirrel() {
+    // instance variables
     var x = mouseX;
     var y = mouseY;
+    var diameter = 200;
 
+    this.getDistance = function(other) {
+        var dist = Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
+        return dist;
+    }
+    
+    this.eat = function() {
+        console.log('try to eat');
+        for(var i = 0; i < numFood; i++) {
+            var food = feed[i];
+            var d = this.getDistance(food);
+            var r1 = food.foodSize / 2;
+            var r2 = diameter / 2;
+            if(r1 + r2 > d) {
+                console.log('hit');
+            }
+        }
+    }
+    
+    this.display = function() {   
+        x = mouseX;
+        y = mouseY;
+    
     // earLeft;
     fill('#282828');
     triangle(x - 25, y - 90, x - 90, y - 20, x - 80, y - 120);
@@ -44,4 +105,5 @@ function draw() {
     fill('#a0a0a0');
     triangle(x, y + 20, x - 20, y + 40, x, y + 30);
     triangle(x, y + 20, x + 20, y + 40, x, y + 30);
+    }
 }
